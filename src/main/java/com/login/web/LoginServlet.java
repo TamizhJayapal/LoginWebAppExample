fbdf70f54040e91a7ed76443d813faccdcfbfd9a
TamizhJayapal <ajtamizh96@gmail.com>
2023-10-19 01:26:21 +0530

    added login register files

--- src/main/java/com/login/web/LoginServlet.java
+++ src/main/java/com/login/web/LoginServlet.java
@@ -1,35 +1,34 @@
 package com.login.web;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-    public LoginServlet() {
-        super();
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public LoginServlet() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		response.setContentType("text/html");
-		if(email.equals("abc@gmail.com") && password.equals("123")) {
-			response.sendRedirect("profile.html");
-		}else {
+		if (email.equals("abc@gmail.com") && password.equals("123")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/ProfileServlet");
+			dispatcher.forward(request, response);
+		} else {
 			response.sendRedirect("login.html");
 		}
 	}
